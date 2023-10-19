444db05ebce3f26246a9f260ea1fbe54ba719a25
Raman Temre <rtemre@systango.com>
2023-10-19 18:24:40 +0530

    Added Fluent UI with Button component

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { DefaultButton } from "@fluentui/react";
+// import { DefaultButton } from "@fluentui/react";
 import Header from "./Header";
 import HeroList, { HeroListItem } from "./HeroList";
 import Progress from "./Progress";
+import { ButtonExample } from "./Button";
 
 /* global Word, require */
 
@@ -75,12 +76,13 @@ export default class App extends React.Component<AppProps, AppState> {
       <div className="ms-welcome">
         <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
         <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
+          <ButtonExample />
+          {/* <p className="ms-font-l">
             Modify the source files, then click <b>Run</b>.
           </p>
           <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
             Run
-          </DefaultButton>
+          </DefaultButton> */}
         </HeroList>
       </div>
     );
--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -1,13 +1,9 @@
 import App from "./components/App";
-import { AppContainer } from "react-hot-loader";
-import { initializeIcons } from "@fluentui/font-icons-mdl2";
-import { ThemeProvider } from "@fluentui/react";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 /* global document, Office, module, require */
-
-initializeIcons();
 
 let isOfficeInitialized = false;
 
@@ -15,11 +11,9 @@ const title = "Contoso Task Pane Add-in";
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <ThemeProvider>
-        <Component title={title} isOfficeInitialized={isOfficeInitialized} />
-      </ThemeProvider>
-    </AppContainer>,
+    <FluentProvider theme={webLightTheme}>
+      <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+    </FluentProvider>,
     document.getElementById("container")
   );
 };
